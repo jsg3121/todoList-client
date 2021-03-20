185657101428855323423a4973559bd07fabcf11
jsg3121 <xodm95@gmail.com>
2021-03-20 15:16:57 +0900

    FEAT(newAccount) : 회원가입 레이아웃 추가

--- src/components/src/newAccount/newAccount.tsx
+++ src/components/src/newAccount/newAccount.tsx
@@ -1,9 +1,67 @@
+import { Button, Form, Input, Select, Space } from 'antd';
 import React from 'react';
 
 const newAccount = () => {
+
+  const getYear = new Date().getFullYear();
+
   return (
-    <div>
-      회원 가입
+    <div className="newAccount-container">
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+      >
+        <Form.Item
+          label="아이디"
+          name="아이디"
+          rules={[{ required: true, message: 'Please input your password!' }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="비밀번호"
+          name="비밀번호"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item label="생년월일">
+          <Space>
+            <Select defaultValue="Year">
+              {
+                [...Array(50)].map((list, index) => {
+                  return (
+                    <Select.Option value={getYear - index}>{getYear - index}</Select.Option>
+                  );
+                })
+              }
+            </Select>
+            <Select defaultValue="Month">
+              {
+                [...Array(12)].map((list, index) => {
+                  return (
+                    <Select.Option value={index + 1}>{index + 1}</Select.Option>
+                  );
+                })
+              }
+            </Select>
+            <Select defaultValue="Day">
+              {
+                [...Array(31)].map((list, index) => {
+                  return (
+                    <Select.Option value={index + 1}>{index + 1}</Select.Option>
+                  );
+                })
+              }
+            </Select>
+          </Space>
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 2 }}>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
--- src/route/src/login/Login.tsx
+++ src/route/src/login/Login.tsx
@@ -1,69 +1,9 @@
-import { Button, Checkbox, Form, Input, Space } from 'antd';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { LoginComponent } from "../../../components";
 import "../../../style/login.scss";
 
 const Login = () => {
-
-  const layout = {
-    labelCol: { span: 6 },
-    wrapperCol: { span: 16 },
-  };
-  const tailLayout = {
-    wrapperCol: { offset: 6, span: 16 },
-  };
-
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
-
   return (
-    <div className="login-container">
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ message: 'Please input your username!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Space size={"small"}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-            <Link to="/newAccount">
-              <Button type={"default"}>
-                회원가입
-            </Button>
-            </Link>
-          </Space>
-        </Form.Item>
-      </Form>
-    </div>
+    <LoginComponent />
   );
 };
 
