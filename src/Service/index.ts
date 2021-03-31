ae3bc192412f5794dd8982f14dbb6aeddfdc94da
jsg3121 <xodm95@gmail.com>
2021-03-31 10:19:54 +0900

    FEAT(crypto) : 비밀번호 암호화 작업

--- src/Service/index.ts
+++ src/Service/index.ts
@@ -1,23 +1,43 @@
 import http from "axios";
 import { newAccountType } from "../types/account.types";
+import CryptoJS from "crypto-js";
+
+const AUTH_KEY = "todolist-made-by-sg";
 
 const addUserService = async (userInfo: newAccountType) => {
+  const encrypted = CryptoJS.AES.encrypt(userInfo.password, AUTH_KEY);
+  const userData = { ...userInfo, password: encrypted.toString() };
+
   await http
-    .post("/api/addUser", {
-      id: userInfo.id,
-      password: userInfo.password,
-      nickName: userInfo.nickName,
-      birthDate: userInfo.birthDate || "",
-      email: userInfo.email || "",
-      phoneNumber: userInfo.phoneNumber || "",
+    .request({
+      method: "POST",
+      url: "/api/checkUser",
+      data: {
+        id: userInfo.id,
+      },
     })
     .then((res) => {
-      if (res.data) {
-        return true;
+      if (!res.data) {
+        return false;
       }
     });
 
-  return false;
+  // await http
+  //   .post("/api/addUser", {
+  //     id: userData.id,
+  //     password: userData.password,
+  //     nickName: userData.nickName,
+  //     birthDate: userData.birthDate || "",
+  //     email: userData.email || "",
+  //     phoneNumber: userData.phoneNumber || "",
+  //   })
+  //   .then((res) => {
+  //     if (res.data) {
+  //       return true;
+  //     }
+  //   });
+
+  // return false;
 };
 
 const getIdCheck = async () => {};
